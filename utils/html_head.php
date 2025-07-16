3c65ec622bfef27bd38643c5654989f4edcb4128
mannuccimattia <mannuccimattia@gmail.com>
2025-07-16 18:19:39 +0200

    [FEAT] Moved common html head to utils php file

---
+++ utils/html_head.php
@@ -0,0 +1,6 @@
+<?php
+echo '<meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.7/js/bootstrap.min.js" integrity="sha512-zKeerWHHuP3ar7kX2WKBSENzb+GJytFSBL6HrR2nPSR1kOX1qjm+oHooQtbDpDBSITgyl7QXZApvDfDWvKjkUw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <link rel="stylesheet" href="./css/style.css">';
